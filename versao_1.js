1081aaf2ec6b095fcd4c12bc9a4fd471f514dd90
Gilson Sebastião <gilsoncbsjr@gmail.com>
2023-08-28 15:42:27 -0300

    Versão 1 e versão 2 final

--- versao_1.js
+++ versao_1.js
@@ -1,20 +1,25 @@
+let cartaJogador = []
+let cartaComputador = []
 if (confirm("Boas Vindas ao Jogo BlackJack \nQuer iniciar uma nova rodada?")) {
-   const cartaJogador = comprarCarta()
-   let pontuacaoJogador = cartaJogador.valor+cartaJogador.valor
-   console.log(`Usuário - cartas:${cartaJogador.texto}${cartaJogador.texto} - pontuação ${pontuacaoJogador}` );
-   const cartaComputador = comprarCarta()
-   let pontuacaoComputador = cartaComputador.valor+cartaComputador.valor
-   console.log(`Coputador - cartas:${cartaComputador.texto}${cartaComputador.texto} - pontuação ${pontuacaoComputador}` );
+   cartaJogador.push(comprarCarta())
+   cartaJogador.push(comprarCarta())
+   let pontuacaoJogador = cartaJogador[0].valor+cartaJogador[1].valor
+   console.log(`Usuário - cartas:${cartaJogador[0].texto}${cartaJogador[1].texto} - pontuação ${pontuacaoJogador}` );
+   
+   cartaComputador.push(comprarCarta())
+   cartaComputador.push(comprarCarta())
+   let pontuacaoComputador = cartaComputador[0].valor+cartaComputador[1].valor
+   console.log(`Coputador - cartas:${cartaComputador[0].texto}${cartaComputador[1].texto} - pontuação ${pontuacaoComputador}` );
 
-   if ((pontuacaoJogador>pontuacaoComputador) && (pontuacaoJogador<21) || (pontuacaoComputador>21)) {
-      console.log("O Usuario ganhou!") 
-   } else if ((pontuacaoComputador>pontuacaoJogador) && (pontuacaoComputador <21) || (pontuacaoJogador>21)) {
-      console.log("O computador ganhou")
+   if ((pontuacaoJogador>pontuacaoComputador) && (pontuacaoJogador<=21) || (pontuacaoComputador>21)) {
+      console.log("O Usuario ganhou!");
+   } else if ((pontuacaoComputador>pontuacaoJogador) && (pontuacaoComputador <=21) || (pontuacaoJogador>21)) {
+      console.log("O computador ganhou");
    } else {
-      console.log("Empate!")
+      console.log("Empate!");
    }
 } else {
-   console.log("O jogo acabou")
+   console.log("O jogo acabou");
 }
 
 
--- versao_2.js
+++ versao_2.js
@@ -1,3 +1,92 @@
+let usuario = []
+let computador = []
+
+if (confirm("Boas Vindas ao Jogo BlackJack \nQuer iniciar uma nova rodada?")) {
+   let cartaInicial = false
+   while (!cartaInicial) {
+      usuario.push(comprarCarta())
+      usuario.push(comprarCarta())
+      computador.push(comprarCarta())
+      computador.push(comprarCarta())
+      if ((usuario[0].valor===11 && usuario[1].valor===11)||(computador[0].valor===11 && computador[1].valor===11)) {
+         usuario = []
+         computador = []
+      } else {
+         cartaInicial = true
+      }
+   }
+
+   let comprando = false
+   let textoUsuario = ''
+   let valorUsuario = 0
+   while (!comprando) {
+      textoUsuario = ''
+      valorUsuario = 0
+      for (carta of usuario) {
+            textoUsuario +=  carta.texto 
+            valorUsuario = valorUsuario + carta.valor
+         }
+         if (valorUsuario>=21) {
+            comprando = true
+         }  else if (confirm(`Suas cartas são ${textoUsuario}. A carta revelada do computador é ${computador[0].texto}.`+`\n`+`\n`+`Deseja comprar mais uma carta?`)) {
+            usuario.push(comprarCarta())
+         
+         }  else {
+            comprando = true
+         }
+   }   
+   
+   let comprandoComputador = false
+   let textoComputador = ''
+   let valorComputador = 0
+   while(!comprandoComputador) {
+      textoComputador = ''
+      valorComputador = 0
+         
+      for (carta of computador) {
+         textoComputador += carta.texto
+         valorComputador = valorComputador + carta.valor
+      }
+      if (valorComputador<21 && valorComputador<valorUsuario && valorUsuario<21) {
+         computador.push(comprarCarta())
+      } else {
+         comprandoComputador = true
+      }
+      
+   }
+
+   let resultado = ''
+   if ((valorComputador>valorUsuario) && (valorComputador<=21) || (valorUsuario>21)) {
+      resultado = `O computador ganhou!`
+   } else if ((valorUsuario>valorComputador) && (valorUsuario<=21) || (valorComputador>21)){
+      resultado = `O usuário ganhou!`
+   } else {
+      resultado = `Empate!`
+   }
+   
+   alert(`Suas cartas são ${textoUsuario}. Sua pontuação é ${valorUsuario}.\nAs cartas do computador são ${textoComputador}. A pontuação do computador é ${valorComputador}.\n${resultado}`)
+   
+} else {
+   console.log("O jogo acabou")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
  * 
